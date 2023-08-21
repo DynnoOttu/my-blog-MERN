7a8c46a37e34ff2d192cf6e5839d188ea9f67cec
DynnoOttu <dynnoottu968@gmail.com>
2023-08-21 15:19:41 +0800

    GET data api dari backend

--- src/component/moleculs/blogitem/index.jsx
+++ src/component/moleculs/blogitem/index.jsx
@@ -1,23 +1,18 @@
 import React from "react";
-import { RegisterBg } from "../../../assets";
 import "./blogitem.scss";
 
-const BlogItem = () => {
+const BlogItem = (props) => {
+  const { image, title, name, date, body } = props;
   return (
     <div className="blog-item">
-      <img className="image-thumb" src={RegisterBg} alt="image-blog" />
+      <img className="image-thumb" src={image} alt="image-blog" />
       <div className="content-detail">
         <div className="">
-          <p className="title">Title</p>
-          <p className="author">Date Post</p>
-          <p className="body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-            perspiciatis necessitatibus sed ad alias, maiores officia qui at
-            quas quod suscipit quia accusamus laboriosam a. Tempora dolores quos
-            cumque itaque natus placeat. Debitis ex impedit reprehenderit quae
-            ut! Beatae, dicta ab ullam consequatur nihil quidem repudiandae
-            blanditiis explicabo dolore possimus?
+          <p className="title">{title}</p>
+          <p className="author">
+            {name} - {date}
           </p>
+          <p className="body">{body}</p>
         </div>
       </div>
     </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BlogItem, Button, Gap } from "../../component";
 import "./home.scss";
+import axios from "axios";
+import { useState } from "react";
 
 const Home = () => {
+  const [dataBlog, setDataBlog] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/v1/blog/posts?page=2&perPage=2")
+      .then((result) => {
+        console.log("DATA API", result);
+        const responseAPI = result.data;
+        setDataBlog(responseAPI.data);
+      })
+      .catch((err) => {
+        console.log("ERROR API", err);
+      });
+  }, []);
   return (
     <div className="home-page-wrapper">
       <div className="create-wrapper">
@@ -10,10 +26,18 @@ const Home = () => {
       </div>
       <Gap height={25} />
       <div className="conetnt-wrapper">
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        {dataBlog.map((blog) => {
+          return (
+            <BlogItem
+              key={blog._id}
+              image={`http://localhost:4000/${blog.image}`}
+              title={blog.title}
+              body={blog.body}
+              name={blog.author.name}
+              date={blog.createdAt}
+            />
+          );
+        })}
       </div>
       <Gap height={25} />
       <div className="pagination">
