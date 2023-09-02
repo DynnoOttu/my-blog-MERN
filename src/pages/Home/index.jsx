15033708b5f4a491e211cb57e55f45fe87b6fc4c
DynnoOttu <dynnoottu968@gmail.com>
2023-09-02 13:56:59 +0800

    pagination

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,30 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BlogItem, Button, Gap } from "../../component";
 import "./home.scss";
 import { setDataBlog } from "../../config/redux/action";
 
 const Home = () => {
-  const { dataBlog } = useSelector((state) => state.homeReducer);
+  const [counter, setCounter] = useState(1);
+  const { dataBlog, page } = useSelector((state) => state.homeReducer);
   const dispatch = useDispatch();
 
+  console.log("PAGE", page);
+
   useEffect(() => {
-    dispatch(setDataBlog());
-  }, []);
+    dispatch(setDataBlog(counter));
+  }, [counter, dispatch]);
+
+  const previous = () => {
+    setCounter(counter <= 1 ? 1 : -1);
+    console.log(counter);
+  };
+
+  const next = () => {
+    setCounter(counter === page.totalPage ? page.totalPage : counter + 1);
+    console.log(counter);
+  };
 
   return (
     <div className="home-page-wrapper">
@@ -35,9 +48,17 @@ const Home = () => {
       </div>
       <Gap height={25} />
       <div className="pagination">
-        <Button title="Previous" />
+        <Button
+          title="Previous"
+          onClick={previous}
+          style={{ marginRight: "20px" }}
+        />
         <Gap width={20} />
-        <Button title="Next" />
+        <p className="text-page">
+          {page.currentPage} / {page.totalPage}
+        </p>
+        <Gap width={20} />
+        <Button title="Next" onClick={next} style={{ marginLeft: "20px" }} />
       </div>
       <Gap height={20} />
     </div>
